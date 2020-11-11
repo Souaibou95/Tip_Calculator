560b52af7e39a44942c0b85438f8c17144dd5f97
Mamadou Diallo <dmsouaibou@gmail.com>
2020-11-10 23:26:42 -0500

    Change Tip and Bill amount to two decimal places

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let btn = document.getElementById("btn");
 btn.addEventListener('click', function(){
   let bill_amount = document.getElementById("bill_amount").value;
   let tip_percentage = document.getElementById("tip_percentage").value;
-  let tip_amount = document.getElementById("tip_amount").value =
-                    bill_amount / tip_percentage;
-  let total_bill = document.getElementById("total_bill").value =
+  let tipAmount = bill_amount / tip_percentage;
+  let tip_amount = document.getElementById("tip_amount").value = tipAmount.toFixed(2);
+
+  let totalBill = document.getElementById("total_bill").value =
                     parseFloat(bill_amount) + parseFloat(tip_amount);
+  let total_bill = totalBill.toFixed(2);
 
 })
